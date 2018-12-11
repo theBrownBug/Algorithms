63675abc492c0aef812da57142f47fd02fc5abaf
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2018-12-11 18:35:35 +0800

    data structures added

---
+++ DS/Queue.java
@@ -0,0 +1,66 @@
+
+
+class Queue{
+
+    private int size;
+    private int front , rear , capacity ;
+    private double[] array ;
+
+    public Queue(int capacity){
+        this.capacity = capacity ;
+        this.size = front = 0 ;
+        this.rear = capacity - 1 ;
+        this.array = new double[capacity] ;
+    }
+
+    public boolean isFull(Queue queue){
+        return queue.size == queue.capacity ;
+    }
+
+    public boolean isEmpty(Queue queue){
+        return queue.size == 0 ;
+    }
+
+    public void enqueue(double data){
+        if(isFull(this)){
+            System.out.println(" The queue is full , overflow ..") ;
+            return  ;
+        }
+        else{
+            this.rear  = (this.rear + 1 )% this.capacity  ;
+            this.array[this.rear] = data  ;
+            this.size += 1 ;
+            System.out.println(data + "is enqueud to the queue") ;
+        }
+    }
+
+    public double dequeue(){
+        if(isEmpty(this)){
+            System.out.println("queue underflow") ;
+            return DOUBLE.MIN_VALUE ;
+        }
+        else{
+            double item = this.array[this.front] ;
+            this.front = (this.front+1)%this.capacity ;
+            this.size -- ;
+            return item ;
+        }
+        return DOUBLE.MIN_VALUE  ;
+    }
+
+    public int getRear(){
+        if(isEmpty(this)){
+            return INTEGER.MIN_VALUE ;
+        }
+        else{return this.rear ; }
+    }
+
+    public int getFront() {
+        if(isEmpty(this)){
+            return INTEGER.MIN_VALUE  ;
+        }
+        else{return this.front;}
+    }
+
+
+}
---
+++ DS/Stack.java
@@ -0,0 +1,74 @@
+
+import java.util.* ;
+public class Stack{
+
+    private int top ;
+    private int size ;
+    private double[] array ;
+
+    public Stack(int size){
+        try {
+            this.size = size;
+            this.top = -1;
+            this.array = new double[size];
+        }catch(NumberFormatException e){
+
+            System.out.println("Enter the correct size of the stack ");
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.top <0 ;
+
+    }
+
+    public boolean push(double data){
+        if(top >=(this.size -1 )){
+
+            System.out.println("There is a stack overflow ") ;
+            return false;
+        }
+        else{
+            this.array[top+1] = data ;
+            top++ ;
+        }
+    }
+
+    public double pop(){
+        if(top<0) {
+            System.out.println("Stack underflow");
+            return DOUBLE.POSITIVE_INFINITY ;
+        }
+        else{
+            return array[top];
+            top-- ;
+        }
+
+    }
+
+    public boolean stack_empty(){
+        return top<0  ;
+
+    }
+
+    public boolean stack_full(){
+        return top== size-1 ;
+    }
+
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
